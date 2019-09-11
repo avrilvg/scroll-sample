3156bf77277fabc4a4e97d1afa359e790be6842c
Abril Veliz <abril.veliz@softvision.com>
2019-09-11 01:37:02 -0300

    Updates

---
+++ precache-manifest.ca9f360527e779b8753125d661331124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc7c3fc7c9b9a18a9da0ecc2ffe5e56",
+    "url": "/scroll-sample/index.html"
+  },
+  {
+    "revision": "1d5ffc08981407a2f04f",
+    "url": "/scroll-sample/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a85d329e560e2fe08770",
+    "url": "/scroll-sample/static/js/2.dfd2a23e.chunk.js"
+  },
+  {
+    "revision": "1d5ffc08981407a2f04f",
+    "url": "/scroll-sample/static/js/main.91e22f22.chunk.js"
+  },
+  {
+    "revision": "0dddf2cb9613e9ea8aba",
+    "url": "/scroll-sample/static/js/runtime~main.a7e9e3c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scroll-sample/static/media/logo.5d5d9eef.svg"
+  }
+]);
